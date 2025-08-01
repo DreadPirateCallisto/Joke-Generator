0c5a53b8c3eebb7a43b55c3f0901ba7ba9661fd6
DreadPirateCallisto <mzee.radi77@icloud.com>
2025-08-01 06:03:07 +0200

    axios get method and fetch joke from first time render

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 import Button from './Button';
 import './Joke.css'
@@ -6,12 +7,27 @@ import './Joke.css'
 function Joke() {
 
 	const [Joke, setJoke] = useState("");
+	const url = "https://sv443.net/jokeapi/v2/joke/Programming?type=single";
 
-	const handleFetchJokes = () => {
-		fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
-			.then((res) => res.json())
-			.then((data) => setJoke(data.joke));
+	const handleFetchJokes = async () => {
+		// fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
+		// 	.then((res) => res.json())
+		// 	.then((data) => setJoke(data.joke));
+
+		try {
+			const result = await axios.get(url);
+
+			setJoke(result.data.joke);
+		} catch {
+			console.log('error');
+		}
 	};
+
+	// fetches jokes first time component renders
+	useEffect(() => {
+		handleFetchJokes();
+	  }, [])
+	
 
   return (
 	  <div className='joke'>
